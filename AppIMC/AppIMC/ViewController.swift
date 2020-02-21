6158f3abcf4bc57500b1d204aec98e9ded8bef5a
MigueAJM9724 <miguejm@outlook.es>
2020-02-20 23:31:16 -0800

    IMC solo se muestra el imc en un label dentro del mismo viewController

---
+++ AppIMC/AppIMC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppIMC
+//
+//  Created by MIGUEL ANGEL JIMENEZ MELENDEZ on 20/02/20.
+//  Copyright © 2020 MIGUEL ANGEL JIMENEZ MELENDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
